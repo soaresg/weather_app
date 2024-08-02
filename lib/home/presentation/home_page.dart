34548cb88076397b928cba1425a75aef9a33816b
Gabriel Soares <gabriel.soares@rodosoft.com.br>
2024-08-02 17:49:40 -0300

    feat: creation of geolocation

--- lib/home/presentation/home_page.dart
+++ lib/home/presentation/home_page.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:weather_app/core/models/weather_info_entity.dart';
 import 'package:weather_app/home/home_controller.dart';
 
 class HomePage extends StatefulWidget {
-  HomePage({super.key});
+  const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -14,22 +15,46 @@ class _HomePageState extends State<HomePage> {
 
   @override
   void initState() {
-    controller.getWeather();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.blueGrey.withOpacity(0.7),
-        child: Center(
-          child: ElevatedButton(
-            onPressed: () async => await controller.getWeather(),
-            child: const Text('Get Current Weather'),
-          ),
-        ),
-      ),
+      body: FutureBuilder(
+          future: controller.getWeather(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.waiting:
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              case ConnectionState.done:
+                if (!snapshot.hasData) {
+                  return const Center(
+                    child: Text('Erro ao buscar a localização atual'),
+                  );
+                } else {
+                  WeatherInfoEntity? currentWeather =
+                      controller.todaysWeather();
+
+                  return Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                            'Latitude: ${controller.position!.latitude} \n Longitude: ${controller.position!.longitude}'),
+                        const SizedBox(height: 8),
+                        Text(
+                            'Current Weather: ${currentWeather!.weather.main}'),
+                      ],
+                    ),
+                  );
+                }
+              default:
+                return Container();
+            }
+          }),
     );
   }
 }
